a34f5d0bfd75d76b6bf5830d534e2fa719b113db
stephendedalus46 <cieciur@onet.pl>
2020-04-11 14:32:03 +0200

    database/migrations/posts added

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()  # text field, better for more text
+    date_posted = models.DateTimeField(default=timezone.now)  # don't execute this function
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # on delete, when user is deleted, it's delete all user's comments
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # no longer being used
+from .models import Post
 
 # Here functions will handle the traffic for the specific routes
 # we need to map our url pattern to those functions (we do it in urls.py, which we create by ourself in blog directory)
 
-# dummy data for a sec
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)  # it's possible to do it with HttpResponse, but it's shorter that way
 
