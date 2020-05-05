d9a1e9bf0c053c4c0d2a2282cae60515b066bc62
stephendedalus46 <cieciur@onet.pl>
2020-05-05 14:38:41 +0200

    Update profile/resize image

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,3 +9,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:  # create a model we want to interact with
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:  # create a model we want to interact with
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
