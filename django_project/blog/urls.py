22230b08f2c20f44d946c4f70e81e4a1fc502cbf
stephendedalus46 <cieciur@onet.pl>
2020-06-24 14:04:00 +0200

    add pagination on home page and specific user page

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path  # copied from django_project
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView  # our classes
+from .views import (  # our classes
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
 from . import views  # we need to have access to functions from views
 
 # copied from django_project
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),  # empty path stays for home route, name should be more specific then home
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),  # pk stands for primary key
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
